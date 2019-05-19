0dc59b09e85a5a9ccb1c764f1d2499175b3b681b
travis <abe.travis@outlook.com>
2019-05-19 14:13:34 -0700

    added java created p ids

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,16 @@
 var userguess = "Your Guesses so far"
+var wins = "0"
+var losses = "0"
+
+var directionsText = document.getElementById("directions");
+var userChoiceText = document.getElementById("tries");
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+
+var computerguesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+document.onkeyup = function(event) {
+
+    var userguess = event.key;
+
+    var computerguess = computerguesses[Math.floor(Math.random() * computerguesses.length)];
