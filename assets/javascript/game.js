56d40a52e96be19612b7b1233de57789ee7b5299
travis <abe.travis@outlook.com>
2019-05-28 17:50:33 -0700

    added game function and removed setTimeout to not get a 'unknown referenceerror'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,40 @@
-var computerguess = ['a','b','c','d','e','f','g','h','i','j','k','l',
-    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
 var wins = 0;
 var losses = 0;
 var guesses = 9;
 var lettersguessed = [];
+game();
 
-var computerChoice = computerguess[Math.floor(Math.random() * computerguess.length)];
+function game() {
+    var computerguess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ];
+    var computerChoice = computerguess[Math.floor(Math.random() * computerguess.length)];
+    console.log(computerChoice);
 
-console.log(computerChoice)
 
-document.onkeypress = function(event) {
-    var userGuess = event.key;
+    document.onkeypress = function (event) {
+        var userGuess = event.key;
 
-    if(userGuess === computerChoice){
-        alert("You Won! Good Job!")
-        wins++;
-        setTimeout(computerChoice = computerguess[Math.floor(Math.random() * computerguess.length)]);
-        console.log(computerChoice)
-    }else{
-        guesses--;
-        lettersguessed.push(userGuess);
+        if (userGuess === computerChoice) {
+            alert("You Won! Good Job!")
+            wins++;
+            game();
+            console.log(computerChoice)
+        } else {
+            guesses--;
+            lettersguessed.push(userGuess);
+        }
+
+        if (guesses === 0) {
+            alert("You Lost! Keep Trying!")
+            losses++;
+            guesses = 9;
+            lettersguessed = [];
+        }
+
+        document.getElementById('wins').innerHTML = "Wins: " + wins;
+        document.getElementById('losses').innerHTML = "losses: " + losses;
+        document.getElementById('guesses').innerHTML = "Guesses: " + guesses;
+        document.getElementById('lettersguessed').innerHTML = "You Guessed: " + [lettersguessed];
     }
-
-    if(guesses === 0){
-        alert("You Lost! Keep Trying!")
-        losses++;
-        guesses = 9;
-        lettersguessed = [];
-    }
-
-    document.getElementById('wins').innerHTML = "Wins: " + wins;
-    document.getElementById('losses').innerHTML = "losses: " + losses;
-    document.getElementById('guesses').innerHTML = "Guesses: " + guesses;
-    document.getElementById('lettersguessed').innerHTML = "You Guessed: " + [lettersguessed];}
-    
+}
